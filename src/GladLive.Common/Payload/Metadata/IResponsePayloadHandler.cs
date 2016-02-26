ce4b52450b16ad91a43d97a2bb019d4c10446ba0
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-26 00:05:59 -0600

    Added non-payload specific definitions of handlers

--- src/GladLive.Common/Payload/Metadata/IResponsePayloadHandler.cs
+++ src/GladLive.Common/Payload/Metadata/IResponsePayloadHandler.cs
@@ -16,4 +16,15 @@ namespace GladLive.Common
 	{
 
 	}
+
+	/// <summary>
+	/// IoC/Meta-data Marker for Response handlers.
+	/// </summary>
+	/// <typeparam name="TPeerType">Type of the peer.</typeparam>
+	/// <typeparam name="TPayloadType">Type of the payload</typeparam>
+	public interface IResponsePayloadHandler<in TPeerType> : IPayloadHandler<TPeerType>
+		where TPeerType : INetPeer
+	{
+
+	}
 }

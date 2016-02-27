47adb50a7a8652993d80074af3bcfd91eae5cbdb
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-26 22:10:55 -0600

    Added HandlerServices; Changed ChainHandler to Strat

---
+++ src/GladLive.Common/Payload/Handlers/Services/EventPayloadHandlerService.cs
@@ -0,0 +1,24 @@
+﻿using GladNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladLive.Common
+{
+	public class EventPayloadHandlerService<TSessionType> : IEventPayloadHandlerService<TSessionType>
+		where TSessionType : INetPeer
+	{
+		private IPayloadHandlerStrategy<TSessionType> handlerStrat;
+
+		public EventPayloadHandlerService(IPayloadHandlerStrategy<TSessionType> strat)
+		{
+			handlerStrat = strat;
+		}
+
+		public bool TryProcessPayload(PacketPayload payload, IMessageParameters parameters, TSessionType peer)
+		{
+			return handlerStrat.TryProcessPayload(payload, parameters, peer);
+		}
+	}
+}

c0ad695d0053b74dd187de88217e9f3f2f2c8a15
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-20 15:58:09 -0500

    Added missing ResponseCode types

--- src/GladLive.Common.Payloads/ResponseCodes/LoginResponseCode.cs
+++ src/GladLive.Common.Payloads/ResponseCodes/LoginResponseCode.cs
@@ -5,13 +5,37 @@ using System.Text;
 
 namespace GladLive.Common.Payloads
 {
+	//Based on: https://github.com/GladLive/GladLive.Web.Payloads/blob/master/src/GladLive.Web.Payloads.Authentication/Payloads/AuthResponseCode.cs
+	//This is basically the clientside available subset of return codes.
+	/// <summary>
+	/// Indicates the result of a <see cref="LoginRequest"/>.
+	/// </summary>
 	[Serializable]
 	public enum LoginResponseCode : byte //even though this causes boxing in some cases it reduces bandwidth
 	{
+		/// <summary>
+		/// Indicates that the login was successful.
+		/// </summary>
 		Success = 0,
+
+		/// <summary>
+		/// Indicates that the login failed due to the user being banned.
+		/// </summary>
 		Banned,
+
+		/// <summary>
+		/// Indicates that the login failed due to the user account being temporarily locked.
+		/// </summary>
 		Locked,
-		Limited,
-		AccountDoesntExist,
+
+		/// <summary>
+		/// Indicates that the login failed to a general failure.
+		/// </summary>
+		Failed,
+
+		/// <summary>
+		/// Indicates that the login failed due to the authentication server being unreachable or unavailable.
+		/// </summary>
+		AuthServerUnavailable,
 	}
 }
